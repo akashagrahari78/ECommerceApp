0b7192e5391915a6382f81c6b6cc669424b6545e
Akash Agrahari <akashagrahari746@gmail.com>
2025-06-29 23:19:06 +0530

    product Add functionality added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 // import express from "express"
 const express = require("express")
-const { listProduct,addProduct, singleProduct, removeProduct } = require("../controllers/productController.js")
+const { listProduct,addProduct, singleProduct, removeProduct } = require("../controllers/productController.js");
+const upload = require("../middleware/multer.js");
 // import { listProduct,addProduct, singleProduct, removeProduct } from "../controllers/productController.js"
 // import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post("/add", addProduct);
+productRouter.post("/add",upload.fields([{name: 'image1', maxCount: 1}, {name: 'image2', maxCount: 1}, {name: 'image3', maxCount: 1}, {name: 'image4', maxCount: 1}]) ,addProduct);
 productRouter.post("/remove", removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProduct);

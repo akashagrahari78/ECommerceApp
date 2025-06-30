661c07018a14949177d90b27e48dd97ecc252b30
Akash Agrahari <akashagrahari746@gmail.com>
2025-06-30 14:54:23 +0530

    admin pages created

---
+++ admin/src/pages/Add.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+const Add = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default Add

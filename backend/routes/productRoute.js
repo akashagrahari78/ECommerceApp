0bb6613b6e174310b6c8ffbc8d8253feee693072
Akash Agrahari <akashagrahari746@gmail.com>
2025-06-30 13:52:48 +0530

    products api's set

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,13 +2,14 @@
 const express = require("express")
 const { listProduct,addProduct, singleProduct, removeProduct } = require("../controllers/productController.js");
 const upload = require("../middleware/multer.js");
+const { adminAuth } = require("../middleware/adminAuth.js");
 // import { listProduct,addProduct, singleProduct, removeProduct } from "../controllers/productController.js"
 // import upload from "../middleware/multer.js";
 
 const productRouter = express.Router();
 
-productRouter.post("/add",upload.fields([{name: 'image1', maxCount: 1}, {name: 'image2', maxCount: 1}, {name: 'image3', maxCount: 1}, {name: 'image4', maxCount: 1}]) ,addProduct);
-productRouter.post("/remove", removeProduct);
+productRouter.post("/add", adminAuth, upload.fields([{name: 'image1', maxCount: 1}, {name: 'image2', maxCount: 1}, {name: 'image3', maxCount: 1}, {name: 'image4', maxCount: 1}]) ,addProduct);
+productRouter.post("/remove", adminAuth,removeProduct);
 productRouter.post("/single", singleProduct);
 productRouter.get("/list", listProduct);
 
